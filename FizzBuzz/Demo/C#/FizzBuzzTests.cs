c2a90ee298767a8ee00bba328dcecc014f88bd5c
Fernando Cuenca <fernando_cuenca@yahoo.com.ar>
2016-10-12 19:47:53 -0400

    added TDD demo start-point

---
+++ FizzBuzz/Demo/C#/FizzBuzzTests.cs
@@ -0,0 +1,12 @@
+
+using NUnit.Framework;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    public void test()
+    {
+        Assert.Fail("niy");
+    }
+}
---
+++ FizzBuzz/Demo/Java/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void test() 
+    {
+      fail("niy");
+    }
+}
