adf84e47b9b91f3299de3acbc245e750093d3143
Fernando Cuenca <fernando_cuenca@yahoo.com.ar>
2016-10-13 11:54:41 -0400

    Created starting-point for general practice sessions

---
+++ EmptyDojo/C#/TestSuite.cs
@@ -0,0 +1,12 @@
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestSuite
+{
+    [Test]
+    public void test_something()
+    {
+      Assert.That("test", Is.EqualTo("Not implemented yet"));
+    }
+}
